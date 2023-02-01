bde1aec8f04a37aee9c60dbf31b2fa98802a54a1
Evelin Kolev <evelinkolev@vivaldi.net>
2023-02-01 13:01:08 +0200

    Add the Custom Model

---
+++ BlazorMyRide/Shared/Custom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMyRide.Shared
+{
+    public class Custom
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public string Description { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+    }
+}
