65e3c3015c80588f3fa8bf33d9ec8a3abb9fa307
Evelin Kolev <evelinkolev@vivaldi.net>
2023-02-04 11:09:03 +0200

    Inject HttpClient & NavigationManager class into CustomService for URI navigation

--- BlazorMyRide/Client/Services/CustomService.cs
+++ BlazorMyRide/Client/Services/CustomService.cs
@@ -1,6 +1,43 @@
-﻿namespace BlazorMyRide.Client.Services
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorMyRide.Client.Services
 {
     public class CustomService : ICutomService
     {
+        private readonly HttpClient _httpClient;
+        private readonly NavigationManager _navigationManager;
+
+        public CustomService(HttpClient httpClient, NavigationManager navigationManager)
+        {
+            _httpClient = httpClient;
+            _navigationManager = navigationManager;
+        }
+
+        public List<Custom> Customs { get; set; } = new List<Custom>();
+
+        public Task AddCustom(Custom custom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCustom(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Custom?> GetCustomById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetCustoms()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCustom(int id, Custom custom)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
