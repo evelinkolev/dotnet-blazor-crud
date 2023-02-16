f537b559c697e0714cb0d5b3b5d4638c3f28211e
Evelin Kolev <evelinkolev@vivaldi.net>
2023-02-16 09:37:53 +0200

    Add the Car Model

---
+++ BlazorMyRide/Shared/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMyRide.Shared
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? VIN { get; set; }
+        public string? LicensePlate { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public string? Color { get; set; }
+        public Custom? Custom { get; set; }
+        public int? CustomId { get; set; }
+    }
+}
