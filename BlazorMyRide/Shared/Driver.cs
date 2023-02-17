b9df9c4381fd35b26a154b7a5ab79a77e9c75d8d
Evelin Kolev <evelinkolev@vivaldi.net>
2023-02-17 09:29:23 +0200

    Add the Driver Model

---
+++ BlazorMyRide/Shared/Driver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorMyRide.Shared
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Gender { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; }
+        public string? NationalCardNumber { get; set; }
+        public string? PIN { get; set; }
+        public int? CarId { get; set; }
+        public Car? Cars { get; set; }
+    }
+}
